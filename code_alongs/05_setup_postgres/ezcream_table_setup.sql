47b69b70a11d57ecb0a098678e01e4f842b92696
Katrin katryl-yh <katrin@rylander.eu>
2025-03-11 12:05:34 +0100

    create customer and orders tables for ezecream

---
+++ code_alongs/05_setup_postgres/ezcream_table_setup.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS ezecream_05;
+
+SET search_path TO ezecream_05;
+
+CREATE TABLE IF NOT EXISTS Customer (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    personal_nubmer CHAR(12),
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
+
+-- SELECT * FROM ezecream_05.customer; 
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL ,
+    order_date DATE DEFAULT CURRENT_TIMESTAMP ,
+    FOREIGN KEY (customer_id) REFERENCES Customer (customer_id)
+);
+
+-- TO DO: implement the rest of the physical logical diagram
---
+++ code_alongs/05_setup_postgres/insert_customer.sql
@@ -0,0 +1,56 @@
+SET search_path TO ezecream_05;
+
+INSERT INTO Customer (first_name, last_name, email, address)
+VALUES
+    (
+        'Ragnar',
+        'Lothbrok',
+        'ragnar@kattegat.com',
+        '1 Valhalla Way'
+    ),
+    (
+        'Lagertha',
+        'Lothbrok',
+        'lagertha@kattegat.com',
+        '2 Shieldmaiden Road'
+        
+    ),
+    (
+        'Bjorn',
+        'Ironside',
+        'bjorn@kattegat.com',
+        '3 Viking Bay'
+        
+    ),
+    (
+        'Ivar',
+        'the Boneless',
+        'ivar@danes.com',
+        '4 The Great Heathen Army Camp'
+        
+    );
+
+SELECT
+    *
+FROM
+    ezecream_05.customer;
+
+
+INSERT INTO ezecream_05.Customer (customer_id, first_name, last_name, email, address)
+VALUES
+    (
+        5,
+        'Ubbe',
+        'Lothbrok',
+        'ragnar@kattegat.com',
+        '1 Valhalla Way'
+    );
+
+SELECT
+    *
+FROM
+    ezecream_05.customer;
+
+-- DELETE FROM ezecream_05.customer WHERE customer_id = 5;
+
+-- TODO: for reader find out how to delete a customer with ID = 50;
